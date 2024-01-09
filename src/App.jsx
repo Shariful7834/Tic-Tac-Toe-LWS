f84c48d8425223a5c6db4cb97964fc5758773697
Md Shariful Islam <engr.shariful.cse@gmail.com>
2024-01-09 19:35:33 +0100

    Creating brand new tic tac toe game using react

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+export default function Square() {
+  return (
+    <>
+      <div>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 text-xl leading-9 h-12 w-12">
+          X
+        </button>
+      </div>
+    </>
+  );
+}
